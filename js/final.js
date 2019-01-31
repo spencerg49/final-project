667d79903494100ef1b22a53ad93529fb1ef45fb
Spencer Guerreiro <spencer.guerreiro@gmail.com>
2019-01-30 19:02:12 -0500

    Added More JS

--- js/final.js
+++ js/final.js
@@ -1,4 +1,8 @@
 var modalElement = document.getElementById('modal');
+var examPic = document.getElementById('examplePic');
+
+
+
 
 document.getElementById('webJava').addEventListener('click', function () {
   modalElement.style.display = "block";
@@ -22,5 +26,19 @@ document.getElementById('actJava').addEventListener('click', function () {
 
 document.getElementById('close').addEventListener('click', function() {
     modalElement.style.display = "none";
-
+    examPic.setAttribute('src',`images/graph.jpg`);
 });
+
+var imageNumber = function (image=1) {
+  var image = Math.floor( Math.random() * 4 ) + 1;
+
+  console.log(`You rolled: ${image}`);
+
+  var imageElement = document.getElementById('image');
+  var nextElement = document.getElementById('nextBtn');
+
+
+
+};
+
+document.getElementById('nextBtn').addEventListener('click', imageNumber);
